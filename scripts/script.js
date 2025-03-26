f14dff94b897f00af86c2f95f5e44911d2e2f7d6
Asteroidus <asteroidus@protonmail.com>
2025-03-26 18:06:57 +0100

    Change Header css

--- scripts/script.js
+++ scripts/script.js
@@ -13,47 +13,31 @@ function smoothScroll(linkId, targetId) {
 
 // Hamburger Menu Functionality
 const body = document.body;
-const header = document.getElementById("mainHeader");
+const header = document.getElementById("header");
 const headerBackground = document.getElementById("headerBackground");
-const headerTitle = document.getElementById("headerTitle");
 const headerLinks = document.getElementById("headerLinks");
 const headerLink = document.querySelectorAll(".header-link");
 const hamburgerMenu = document.getElementById("hamburgerMenu");
 
 function toggleHamburgerMenu() {
-    body.classList.toggle("is-hamburger-active");
-    header.classList.toggle("is-hamburger-active");
-    headerBackground.classList.toggle("is-hamburger-active");
-    headerTitle.classList.toggle("is-hamburger-active");
-    headerLinks.classList.toggle("is-hamburger-active");
-    hamburgerMenu.classList.toggle("active");
-
-    if (headerLinks.classList.contains("is-hamburger-active")) {
-        headerLink.forEach((el) => {
-            el.classList.add('is-hamburger-active');
-            body.style.overflow = "hidden";
-        });
-    } else {
-        body.style.overflow = "scroll";
-        headerLink.forEach(el => el.style.display = "none");
-        headerLink.forEach(el => el.classList.remove('is-hamburger-active'));
-        setTimeout(function() {
-            headerLink.forEach(el => el.style.display = "flex");
-        }, 100);
-    }
+    body.classList.toggle("expanded")
+    header.classList.toggle("expanded");
+    headerBackground.classList.toggle("expanded");
+    headerLinks.classList.toggle("expanded");
+    hamburgerMenu.classList.toggle("expanded");
 }
 
 hamburgerMenu.addEventListener("click", toggleHamburgerMenu);
 
 window.addEventListener("resize", () => {
-    if (window.innerWidth > 800 && header.classList.contains("is-hamburger-active")) {
+    if (window.innerWidth > 800 && header.classList.contains("expanded")) {
         toggleHamburgerMenu();
     }
 });
 
 headerLink.forEach((link) => {
     link.addEventListener('click', () => {
-        if (header.classList.contains('is-hamburger-active')) {
+        if (header.classList.contains('expanded')) {
             toggleHamburgerMenu();
         }
     });
