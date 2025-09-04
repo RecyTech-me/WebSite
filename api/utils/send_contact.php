e1eec20c7899799a64a9b558c6772d08165017d5
JdM <julien@rmbi.ch>
2025-09-04 18:30:07 +0200

    test

--- api/utils/send_contact.php
+++ api/utils/send_contact.php
@@ -1,4 +1,8 @@
 <?php
+// Enable error reporting for debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Récupération des données
     $contenu = htmlspecialchars($_POST['contenu'] ?? '');
